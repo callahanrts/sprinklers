33d90622fd5c348ec43d0c6a0d59931b66739dab
cody <callahanrts+sprinklers@gmail.com>
2019-03-31 10:26:52 -0700

    Updating sprinkler schedule

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Sprinklers
 A simple webserver I use to manage my irrigation system.
+
+## Setup
+### Avahi
+Install [avahi](http://elinux.org/RPi_Advanced_Setup) on the raspberry pi. gain ssh access by 
+`ssh {{hostname}}.local`
--- index.js
+++ index.js
@@ -6,15 +6,11 @@ var schedule = require('node-schedule');
 
 var water = function(cron, pin, duration){
   schedule.scheduleJob(cron, function(){
-    exec(__dirname + "pin.sh " +pin+ " "+duration);
+    exec(__dirname + "/pin.sh " +pin+ " "+duration);
   });
 }
 
 var pins = {
-  // "1": 2,
-  // "2": 3,
-  // "3": 4,
-  // "4": 17
   "1": 17,
   "2": 4,
   "3": 3,
@@ -25,10 +21,59 @@ for(i = 1; i <= 4; i++){
   exec(__dirname+"/pin_init.sh " + pins[i.toString()], function(){console.log(arguments)});
 }
 
-// water("0,20,40 * * * * *", pins["1"], "2s");
-// water("5,25,45 * * * * *", pins["2"], "2s");
-// water("10,30,50 * * * * *", pins["3"], "2s");
-// water("15,35,50 * * * * *", pins["4"], "2s");
+// CRON REFERENCE
+// https://crontab.guru
+//
+// *    *    *    *    *    *
+// |    |    |    |    |    |
+// |    |    |    |    |    - day of week (0 - 7) (0 or 7 is Sun)
+// |    |    |    |    - month (1 - 12)
+// |    |    |    - day of month (1 - 31)
+// |    |    - hour (0 - 23)
+// |    - minute (0 - 59)
+// - second (0 - 59, OPTIONAL)
+
+
+
+/*
+ * Spring/Fall Watering 
+ *   - April 1 - June 21
+ *   - April Sept 1 - November ish
+ *
+ * Watering times should be adjusted for each season. Our lawns in northern Nevada
+ * don't necessarily require a lot of water, but the water times and the
+ * minutes we water are important. We over water because we falsely think that our
+ * lawns need a ton of moisture to keep them healthy and lush, or we're just not
+ * taught otherwise. In fact, the opposite is true: short duration watering twice
+ * a week is usually sufficient. In the spring and fall, when the temperatures are
+ * cool, maintaining a green lush lawn is very low maintenance. On your watering
+ * days, water once late in the morning and once late in the afternoon, 10 minutes
+ * at a time. If runoff occurs at 10 minutes, cut it back to 7 minutes. When you
+ * water for short periods at a time, the ground is able to absorb the moisture
+ * right away and doesn't run off, evaporate or blow away. Anytime you have
+ * pools of water, or run off occurs, you're watering too much. Very
+ * shady areas will require minimal water during early spring and late fall. In
+ * the spring and fall, we advise you water your lawn at 10:00 a.m. and 4:00 p.m.
+ *
+ * 10 Minutes at 10:00 AM and 4:00 PM
+ *
+ * */
+
+/*
+ * Spring
+ * Mon/Wed (0,3)
+ *.
+water("0 10 * * 0,3", pins["2"], "15m");
+water("17 10 * * 0,3", pins["3"], "10m");
+water("19 10 * * 0,3", pins["4"], "10m");
+// water("0 16 * * 0,3", pins["2"], "15m");
+// water("17 16 * * 0,3", pins["3"], "10m");
+// water("19 16 * * 0,3", pins["4"], "10m");
+
+
+
+
+
 
 
 app.get('/', function(req, res){
@@ -48,4 +93,30 @@ app.listen(3000, function(){
   console.log('listening on port 3000');
   console.log("Pin Script: "+ __dirname+"/pin.sh");
 });
+
+
+
+
+
+
+
+/* OLD (non-western turf advised) watering schedule */
+/* 
+ * Summer Time
+ * Mon/Wed/Fri (0,3,5) 0.5" each day
+ */
+// water("0  6 * * 0,3,5", pins["2"], "23m");
+// water("25 6 * * 0,3,5", pins["3"], "33m");
+// water("0  7 * * 0,3,5", pins["4"], "33m");
+// water("35 7 * * 0,3,5", pins["2"], "23m");
+
+
+/* 
+ * Fall/Spring Time
+ * Mon/Wed (0, 3) 0.5" each day
+ */
+// water( "0 6 * * 0,3", pins["2"], "20m");
+// water("25 6 * * 0,3", pins["3"], "30m");
+// water( "0 7 * * 0,3", pins["4"], "30m");
+// water("35 7 * * 0,3", pins["2"], "20m");
 
